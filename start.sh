f9e9bbff181dde99d99e1aa34dc59297f78b8a2b
MictoNode <94179917+MictoNode@users.noreply.github.com>
2025-06-01 12:23:59 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
 CONFIG_FILE=/root/pipe/config.json
-POP_BINARY=/root/pipe/pop
 
 echo "Starting POP node with config:"
 cat $CONFIG_FILE
 
-chmod +x $POP_BINARY
-
 export POP_CONFIG_PATH=$CONFIG_FILE
 
-$POP_BINARY
+/usr/local/bin/pop
